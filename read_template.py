2d7b437852a8503b4311ad9d709ea02641171240
Alex Koryavets <AlexK@paraport.com>
2018-01-30 16:47:58 -0800

    Almost there with saving transformation

--- read_template.py
+++ read_template.py
@@ -1,13 +1,15 @@
 #--------------------------------------------------------------------------------------------------
 ##	Imports
+import re
 import sys
 import yaml
 
-#import pyspark
-#from pyspark import SparkConf, SparkContext
-# from pyspark.sql import SQLContext, SparkSession
-# from pyspark.sql.functions import *
-# from pyspark.sql.types import StructType, StructField, DoubleType, IntegerType, StringType
+import pyspark
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext, SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import \
+	StructType, StructField, DoubleType, IntegerType, StringType, DecimalType
 
 
 #--------------------------------------------------------------------------------------------------
@@ -20,7 +22,6 @@ APP_NAME	= 'Spark_Metadata_ETL'
 class pipeline:
 	def __init__(self):
 		self.steps			= {}
-	pass
 
 class pipelinestep:
 	def __init__(self, dict):
@@ -31,14 +32,16 @@ class pipelinestep:
 		self.name			= dict['name']
 		self.description	= dict['description']
 		self.action			= dict['action']
-	pass
 
 class dataobject:
 	def __init__(self, dict):
 		self.columns		= {}
 		self.path			= dict['path']
-		self.types			= dict['type']
-	pass
+		self.format			= dict['format']
+		if 'mode' in dict:
+			self.mode		= dict['mode']
+		else:
+			self.mode		= 'read'
 
 class transformation:
 	def __init__(self, dict):
@@ -48,7 +51,11 @@ class transformation:
 		self.destination_1	= dict['destination 1']
 		self.destination_2	= dict['destination 2']
 
-	pass
+class column:
+	def __init__(self, dict):
+		self.name			= dict['name']
+		self.position		= dict['position']
+		self.datatype		= dict['datatype']
 
 #--------------------------------------------------------------------------------------------------
 ##	Service methods
@@ -60,12 +67,12 @@ def readArray(input, collection):
 		for item in input[collection]:
 			key = next(iter(item))
 			res[key] = item[key]
-	
-	return res;
+
+	return res
 
 #------------------------------------------------
 def readConfig(inputFile):
-	
+
 	#	Result
 	res = {}
 
@@ -76,28 +83,38 @@ def readConfig(inputFile):
 	bufColumns		= {}
 
 	f = open(inputFile, 'r')
-	
+
 	y_file = yaml.load(f)
-	
+
 	bufDict = readArray(y_file, 'pipelines')
-	
+
 	for curPpl in bufDict:
 		objPipeline = pipeline()
 		bufSteps = readArray(bufDict[curPpl], 'steps')
-		
+
 		for curStp in bufSteps:
 			objStep = pipelinestep(bufSteps[curStp])
 
 			bufDataobjects = readArray(bufSteps[curStp], 'sources')
 			for curDS in bufDataobjects:
 				objSource = dataobject(bufDataobjects[curDS])
-				objSource.columns = readArray(bufDataobjects[curDS], 'columns')
+				
+				bufColumns = readArray(bufDataobjects[curDS], 'columns')
+				for curCol in bufColumns:
+					objColumn = column(bufColumns[curCol])
+					objSource.columns[curCol] = objColumn
+
 				objStep.sources[curDS] = objSource
 			
 			bufDataobjects = readArray(bufSteps[curStp], 'destinations')
 			for curDS in bufDataobjects:
 				objDest = dataobject(bufDataobjects[curDS])
-				objDest.columns = readArray(bufDataobjects[curDS], 'columns')
+				
+				bufColumns = readArray(bufDataobjects[curDS], 'columns')
+				for curCol in bufColumns:
+					objColumn = column(bufColumns[curCol])
+					objDest.columns[curCol] = objColumn
+
 				objStep.destinations[curDS] = objDest
 			
 			bufTransformations = readArray(bufSteps[curStp], 'transformations')
@@ -112,16 +129,76 @@ def readConfig(inputFile):
 	return res;
 
 #------------------------------------------------
-def getDataFrame(source):
+def createSchema(inputDataobject, inputDataset):
 
-#	source.
+	res = StructType()
+
+	for curColId in inputDataobject.columns:
+		curCol = inputDataobject.columns[curColId]
+
+		# If given position, schema is initialized from inputDataset
+		if (curCol.position):
+			if 'schema' in dir(inputDataset):
+				curField = inputDataset.schema[curCol.position-1]
+			else:
+				curField = StructField(curColId, StringType())
+#			curField.metadata
+		else:
+			fieldName = curCol.name
+			if (curCol.datatype == 'int'):
+				fieldDataType = IntegerType()
+			elif (curCol.datatype == 'money'):
+				fieldDataType = DecimalType(19, 4)
+			elif (curCol.datatype == 'double'):
+				fieldDataType = DoubleType()
+			else:
+				fieldDataType = StringType()
+			
+			curField = StructField(fieldName, fieldDataType)
+		
+		res.add(curField)
 
 	return res;
 
 #------------------------------------------------
-def loadData(source, destination):
+def getDataFrame(inputDataobject):
 
-	sourceDF = getDataFrame(source)
+	if (inputDataobject.format in ('csv', 'txt')):
+		dataset = sc.textFile(inputDataobject.path).map( lambda x: x.replace('\"',''))
+		dataschema = createSchema(inputDataobject, dataset)
+		res = spark.createDataFrame(dataset, dataschema)
+	else:
+		res = spark.read.load(inputDataobject.path, format=inputDataobject.format)
+		dataschema = createSchema(inputDataobject, res)
+		
+	return res, dataschema;
+
+#------------------------------------------------
+## Will do mapping of input table columns to output table columns
+def mapDataFields(inputDataobject, inputDataschema, outputDataobject, transformations):
+
+	res = ''
+	for curSrcCol in inputDataschema:
+		#	For now no mapping, to be modified soon
+		sourceCol = curSrcCol.name
+		destCol = re.sub('[ ,;{}()\\n\\t=]', '', curSrcCol.name)
+		res += (",", "")[res==''] + "`%s` AS `%s`" % (sourceCol, destCol)
+		
+	return res;
+
+#------------------------------------------------
+def loadData(source, destination, transformations):
+
+	sourceDF, sourceDataschema = getDataFrame(source)
+	sourceDF.createOrReplaceTempView('sourceData')
+
+	sourceDataFields = mapDataFields(source, sourceDataschema, destination, transformations)
+
+	sourceData = spark.sql("SELECT %s FROM sourceData" % sourceDataFields)
+
+	sourceData.coalesce(1).write.save(destination.path, format=destination.format, mode=destination.mode)
+
+#	destinationDF = getDataFrame(destination, 'Write')
 
 	return;
 
@@ -132,9 +209,8 @@ def initializeSpark():
 		.appName(APP_NAME) \
 		.enableHiveSupport() \
 		.getOrCreate()
-	sc = spark.sparkContext
 
-	return sc;
+	return spark;
 
 #--------------------------------------------------------------------------------------------------
 ##	Main
@@ -148,9 +224,15 @@ if __name__ == "__main__":
 
 	try:
 		dctPipelines = readConfig(CONFIG_FILE)
-		print(len(dctPipelines))
 
-#		sc = initializeSpark()
+		spark = initializeSpark()
+		sc = spark.sparkContext
+
+		loadData( \
+			dctPipelines['pipeline 1'].steps['step 1'].sources['source_1'], \
+			dctPipelines['pipeline 1'].steps['step 1'].destinations['destination_1'], \
+			dctPipelines['pipeline 1'].steps['step 1'].transformations['transformation_1'] \
+		)
 
 	except:
 		raise
