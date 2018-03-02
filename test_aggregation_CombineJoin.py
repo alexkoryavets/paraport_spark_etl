2b7b20a96c4a8e62b0134ce5386c62c08c47e2ce
Alex Koryavets <AlexK@paraport.com>
2018-03-02 10:08:40 -0800

    Static partitioning

--- test_aggregation_CombineJoin.py
+++ test_aggregation_CombineJoin.py
@@ -19,8 +19,9 @@ sc = spark.sparkContext
 #   Input parameters
 # main_config_file = "/user/RashiR/Metadata/Metadata_sqoop.txt"
 # columns_config_file = "/user/RashiR/Metadata/Table_schema_metadata.csv"
-main_config_file = "c:\\temp\\hdfs\\config\\Metadata_sqoop.txt"
-columns_config_file = "c:\\temp\\hdfs\\config\\Table_schema_metadata.csv"
+# main_config_file = "c:\\temp\\hdfs\\config\\Metadata_sqoop.txt"
+# columns_config_file = "c:\\temp\\hdfs\\config\\Table_schema_metadata.csv"
+main_config_file = "/user/RashiR/Metadata/Metadata_sqoop.txt"
 #-----------------------------------------------------------------------------
 def convertInt(text):
     if text is not None:
@@ -56,8 +57,9 @@ def convertDatetime(text2):
 if __name__ == "__main__":
 
     workflowStartTime = datetime.datetime.now()
-    if len(sys.argv) > 2:
+    if len(sys.argv) > 1:
         main_config_file = sys.argv[1]
+    if len(sys.argv) > 2:
         columns_config_file = sys.argv[2]
 
     spark.udf.register('udfConvertInt', convertInt, IntegerType())
@@ -65,90 +67,114 @@ if __name__ == "__main__":
     spark.udf.register('udfConvertDatetime', convertDatetime, TimestampType())
 
     mainConfig = spark.read.load(main_config_file, format="csv", delimiter="|", header=True)
-    columnsConfig = spark.read.load(columns_config_file, format="csv", delimiter=",", header=True)
     
     for row in mainConfig.collect():
-        tableName = row.Table
-        keyColumns = [] # ['_c0', '_c2']
-        lastUpdatedColumn = None # '_c4'
-        partitionColumn = row.PartitionColumn # 'partition_column'
-        pathToRaw = row.TargetLocationRaw # "/user/RashiR/data/ccep_constituents_hdfs/Raw"
-        pathToCooked = row.TargetLocationCooked # "/user/RashiR/data/ccep_constituents_hdfs/Cooked"
-        sql = "CREATE EXTERNAL TABLE default." + tableName + "(\n"
-        sqlCols = []
-        outputSchema = []
-        selectSqlTokens = []
-        
-        # Mapping column names from table schema metadata to column numbers in CSV file
-        for curCol in columnsConfig.filter(columnsConfig.Table == tableName).collect():
-            srcColName = '_c' + str(int(curCol.Position) - 1)
-
-            if row.UniqueIdentifiers and row.DeltaColumn:
-                if curCol.Column.lower() in row.UniqueIdentifiers.lower().split(','):
-                    keyColumns.append(srcColName)
-                if curCol.Column.lower() in row.DeltaColumn.lower().split(','):
-                    lastUpdatedColumn = srcColName
+        try:
+            tableName = row.Table
+            keyColumns = [] # ['_c0', '_c2']
+            lastUpdatedColumn = None # '_c4'
+            partitionColumn = row.PartitionColumn # 'partition_column'
+            pathToRaw = row.TargetLocationRaw # "/user/RashiR/data/ccep_constituents_hdfs/Raw"
+            pathToCooked = row.TargetLocationCooked # "/user/RashiR/data/ccep_constituents_hdfs/Cooked"
+            pathToColumnsConfig = row.TargetLocationTableSchema
+            sql = "CREATE EXTERNAL TABLE default." + tableName + "(\n"
+            sqlCols = {}
+            outputSchema = []
+            selectSqlTokens = []
             
-            # !!!TEMP!!! Need to create proper mapping of source column types to destination ones
-            if (curCol.Datatype in ("datetime", "datetime2")):
-                dataframeDatatype = TimestampType()
-                hiveDatatype = "TIMESTAMP"
-                selectSqlTokens.append("TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(`%s`, 'yyyy-MM-dd hh:mm:ss'))) AS `%s`" % (srcColName, curCol.Column))
-            elif (curCol.Datatype == "int"):
-                dataframeDatatype = IntegerType()
-                hiveDatatype = "INT"
-                selectSqlTokens.append("udfConvertInt(`%s`) AS `%s`" % (srcColName, curCol.Column))
-            elif (curCol.Datatype == "double"):
-                dataframeDatatype = DoubleType()
-                hiveDatatype = "DOUBLE"
-                selectSqlTokens.append("udfConvertDouble(`%s`) AS `%s`" % (srcColName, curCol.Column))
-            else:
-                dataframeDatatype = StringType()
-                hiveDatatype = "STRING"
-                selectSqlTokens.append("`%s` AS `%s`" % (srcColName, curCol.Column))
-
-            outputSchema.append(StructField(curCol.Column, dataframeDatatype, curCol.Nullable))
-            sqlCols.append("\t%s %s" % (str(curCol.Column), hiveDatatype))
-        
-        sql = sql + ",\n".join(sqlCols) + ")\n STORED AS PARQUET LOCATION '%s'" % pathToCooked
-        
-        print('Started processing %s at: %s' % (pathToRaw, datetime.datetime.now()))
+            # Mapping column names from table schema metadata to column numbers in CSV file
+            columnsConfig = spark.read.load(pathToColumnsConfig, format="csv", delimiter="|", header=False)
             
-        # df = spark.read.load("C:\\Temp\\hdfs\\Raw", format="csv", delimiter="|")
-        df = spark.read.load(pathToRaw, format="csv", delimiter="|")
-
-        if (len(keyColumns) > 0 and lastUpdatedColumn):
+            for curCol in columnsConfig.filter(columnsConfig._c0 == tableName).collect():
+                srcColName = '_c' + str(int(curCol._c2) - 1)
+                
+                if row.UniqueIdentifiers and row.DeltaColumn:
+                    if curCol._c1.lower() in row.UniqueIdentifiers.lower().split(','):
+                        keyColumns.append(srcColName)
+                    if curCol._c1.lower() in row.DeltaColumn.lower().split(','):
+                        lastUpdatedColumn = srcColName
+                
+                # !!!TEMP!!! Need to create proper mapping of source column types to destination ones
+                if (curCol._c4 in ("datetime", "datetime2")):
+                    dataframeDatatype = TimestampType()
+                    hiveDatatype = "DATE"
+                    selectSqlTokens.append("TO_DATE(FROM_UNIXTIME(UNIX_TIMESTAMP(`%s`, 'yyyy-MM-dd hh:mm:ss'))) AS `%s`" % (srcColName, curCol._c1))
+                elif (curCol._c4 == "int"):
+                    dataframeDatatype = IntegerType()
+                    hiveDatatype = "INT"
+                    selectSqlTokens.append("udfConvertInt(`%s`) AS `%s`" % (srcColName, curCol._c1))
+                elif (curCol._c4 == "double"):
+                    dataframeDatatype = DoubleType()
+                    hiveDatatype = "DOUBLE"
+                    selectSqlTokens.append("udfConvertDouble(`%s`) AS `%s`" % (srcColName, curCol._c1))
+                else:
+                    dataframeDatatype = StringType()
+                    hiveDatatype = "STRING"
+                    selectSqlTokens.append("`%s` AS `%s`" % (srcColName, curCol._c1))
+                
+                outputSchema.append(StructField(curCol._c1, dataframeDatatype, curCol._c3))
+                sqlCols[curCol._c1.lower()] = ("\t%s %s" % (str(curCol._c1), hiveDatatype))
             
-            # Creating mapping group with key = all PK columns concatenated ('pk_column1, ..., pk_columnN'), value = last_updated_date_column
-            #mappedGroup = df.rdd.map(lambda row: (",".join((str(row._c0), str(row._c2))), row._c4))
-            mappedGroup = df.rdd.map(lambda row: (",".join(str(row[kc]) for kc in keyColumns), row[lastUpdatedColumn]))
+            print('Started processing %s at: %s' % (pathToRaw, datetime.datetime.now()))
+                
+            # df = spark.read.load("C:\\Temp\\hdfs\\Raw", format="csv", delimiter="|")
+            df = spark.read.load(pathToRaw, format="csv", delimiter="|")
             
-            # Creating mapping group with key = all PK columns plus last_updated ('pk_column1, ..., pk_columnN, last_updated_date_column'), value = whole_row
-            #mappedAll = df.rdd.map(lambda row: (",".join((str(row._c0), str(row._c2), str(row._c4))) , [row]))
-            mappedAll = df.rdd.map(lambda row: (",".join(str(row[kc]) for kc in keyColumns) + "," + str(row[lastUpdatedColumn]) , [row]))
+            if (len(keyColumns) > 0 and lastUpdatedColumn):
+                
+                # Creating mapping group with key = all PK columns concatenated ('pk_column1, ..., pk_columnN'), value = last_updated_date_column
+                #mappedGroup = df.rdd.map(lambda row: (",".join((str(row._c0), str(row._c2))), row._c4))
+                mappedGroup = df.rdd.map(lambda row: (",".join(str(row[kc]) for kc in keyColumns), row[lastUpdatedColumn]))
+                
+                # Creating mapping group with key = all PK columns plus last_updated ('pk_column1, ..., pk_columnN, last_updated_date_column'), value = whole_row
+                #mappedAll = df.rdd.map(lambda row: (",".join((str(row._c0), str(row._c2), str(row._c4))) , [row]))
+                mappedAll = df.rdd.map(lambda row: (",".join(str(row[kc]) for kc in keyColumns) + "," + str(row[lastUpdatedColumn]) , [row]))
+                
+                # Extracting maximum last_updated_column_value per PK columns combination
+                grouppedFilter = mappedGroup.combineByKey(lambda x: x, lambda x, y: x if x >= y else y, lambda x, y: x if x >= y else y)
+                # Converting ("pk_column1, ..., pk_columnN", last_updated_date_value) => ("pk_column1, ..., pk_columnN, last_updated_date_value", None)
+                grouppedFilterCombined = grouppedFilter.map(lambda row: (",".join(str(c) for c in row), None))
+                
+                # Joining aggregated values with the unaggregated dataset and removing columns belonging to grouppedFilterCombined from the results
+                df = grouppedFilterCombined.join(mappedAll).values().map(lambda row: row[1][0]) #.collect()
+                
+                df = spark.createDataFrame(df)
             
-            # Extracting maximum last_updated_column_value per PK columns combination
-            grouppedFilter = mappedGroup.combineByKey(lambda x: x, lambda x, y: x if x >= y else y, lambda x, y: x if x >= y else y)
-            # Converting ("pk_column1, ..., pk_columnN", last_updated_date_value) => ("pk_column1, ..., pk_columnN, last_updated_date_value", None)
-            grouppedFilterCombined = grouppedFilter.map(lambda row: (",".join(str(c) for c in row), None))
+            df.createOrReplaceTempView("resTempDF")
             
-            # Joining aggregated values with the unaggregated dataset and removing columns belonging to grouppedFilterCombined from the results
-            df = grouppedFilterCombined.join(mappedAll).values().map(lambda row: row[1][0]) #.collect()
-        
-            df = spark.createDataFrame(df)
-
-        df.createOrReplaceTempView("resTempDF")
-        
-        sqlCmd = "SELECT %s FROM resTempDF" % ",".join(selectSqlTokens)
-        resDF = spark.sql(sqlCmd)
-        
-        resDF.write.save(pathToCooked, format="parquet", mode="overwrite", partitionBy=partitionColumn)
-        #resDF.coalesce(1).write.save("C:\\Temp\\hdfs\\Cooked", format="csv", delimiter="|", mode="overwrite")
-        
-        # resDF.write.save("c:\\temp\\hdfs\\output\\test_aggregate_filtered\\", format="parquet", mode="overwrite", partitionBy="LoadDate")
-        
-        print('Finished processing %s at: %s' % (pathToCooked, datetime.datetime.now()))
-        print('SQL to create table (will be replaced when connectivity to Hive metastore is fixed): \n %s' % sql)
+            sqlCmd = "SELECT %s FROM resTempDF" % ",".join(selectSqlTokens)
+            resDF = spark.sql(sqlCmd)
+            
+            resDF.write.save(pathToCooked, format="parquet", mode="overwrite", partitionBy=partitionColumn)
+            realDF = spark.read.load(pathToCooked, format="parquet")
+            
+            # reading real schema in saved document cause saving can move columns when partitioning resulting dataset
+            outputSQLColumns = []
+            partitionBySql = ""
+            for realCol in realDF.schema:
+                if (realCol.name == partitionColumn):
+                    partitionBySql = "PARTITION BY (%s)" % sqlCols[realCol.name.lower()]
+                else:
+                    outputSQLColumns.append(sqlCols[realCol.name.lower()])
+            
+            # Generating Hive SQL to create a table on top of the saved file
+            sql = sql + ",\n".join(outputSQLColumns) + ")\n%s\nSTORED AS PARQUET LOCATION '%s';" % (partitionBySql, pathToCooked)
+            
+            # Adding manual static partitioning cause dynamic one doesn't seem to work
+            if partitionColumn:
+                for curPartition in realDF.select(partitionColumn).distinct().sort(partitionColumn).collect():
+                    #ALTER TABLE default.T_PPA_MASTER_PRICE ADD PARTITION(PRICE_EFFECTIVE_DATE='2018-02-12') location '/user/AlexK/data/T_PPA_MASTER_PRICE/Cooked/PRICE_EFFECTIVE_DATE=2018-02-12';
+                    sql = sql + "\n ALTER TABLE default.%s ADD PARTITION(%s='%s') location '%s/%s=%s';" % (tableName, partitionColumn, curPartition, pathToCooked, partitionColumn, curPartition)
+            
+            #resDF.coalesce(1).write.save("C:\\Temp\\hdfs\\Cooked", format="csv", delimiter="|", mode="overwrite")
+            
+            # resDF.write.save("c:\\temp\\hdfs\\output\\test_aggregate_filtered\\", format="parquet", mode="overwrite", partitionBy="LoadDate")
+            
+            print('Finished processing %s at: %s' % (pathToCooked, datetime.datetime.now()))
+            print('SQL to create table (will be replaced when connectivity to Hive metastore is fixed): \n %s' % sql)
+        except:
+            print('Errors when processing %s' % (tableName))
+            print(str(sys.exc_info()[0]))
         
     workflowEndTime = datetime.datetime.now()    
     message = 'Processing of %i file(s) is done. Started at: %s, ended at %s, total time:%s\n\t' % (mainConfig.count(), workflowStartTime, workflowEndTime, workflowEndTime-workflowStartTime)
